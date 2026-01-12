d964e64f3ca4a51dbe1319bc8a104e143d9b576e
Gerald122099 <gdurano.shs@gmail.com>
2026-01-13 05:38:08 +0800

    inquiry logo

--- client/src/pages/public/MemberInquiryPage.jsx
+++ client/src/pages/public/MemberInquiryPage.jsx
@@ -1,5 +1,5 @@
 
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 import { useState } from "react";
 const API_BASE = import.meta.env.VITE_API_BASE; // http://localhost:5000/api
 
